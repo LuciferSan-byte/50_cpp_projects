0f6717c9867ac9ca6a8d2317140be045ac0e73e0
LuciferSan-byte <apostolescu.alexandru233@gmail.com>
2025-11-11 16:20:29 +0200

    Sistem de hint uri netestat

--- 3.Casino_game/main.cpp
+++ 3.Casino_game/main.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <random>
 #include <thread>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 class Game {
 private:
+  std::vector<int> numere_prime = {2,3,7,11,13}; 
   int prize = 10000;
   int difficulty = 1;
-  int current_score = 0 , final_score = 5;
+  int current_score = 0, final_score = 5, computerChoice;
   int number_range = 5;
   int incercari = 3, stand = 1;
   std::mt19937 gen{std::random_device{}()};
@@ -15,11 +19,13 @@ private:
   int getPlayerChoice() {
     int choice;
     while (true) {
-      std::cout << "Alege un nr de la 1 la " << difficulty * number_range << " ";
-      if(std::cin >> choice &&choice >= 1 && choice <= difficulty * number_range){
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		return choice;
-	    }
+      std::cout << "Alege un nr de la 1 la " << difficulty * number_range
+                << " ";
+      if (std::cin >> choice && choice >= 1 &&
+          choice <= difficulty * number_range) {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return choice;
+      }
       std::cout << "Invalid input\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -27,9 +33,38 @@ private:
   }
   int getCurrScore() { return current_score; }
   int getMaxScore() { return (final_score * difficulty); }
-  bool DecideWin(int Player, int Computer)const{ return  Player == Computer;}
+  bool DecideWin(int Player, int Computer) const { return Player == Computer; }
 
-  void roundResult(int PlayerChoice , int ComputerChoice){
+
+  void patrat_perfect(int computerChoice){
+    if(sqrt(computerChoice) == (int)sqrt(computerChoice))
+	    std::cout << "Nr este patrat perfect";
+    else std::cout << "Nr nu este patrat perfect";
+    }
+  void hint(int computerChoice) {
+    std::uniform_int_distribution<int> hints(1, 5);
+    int hint = hints(gen);
+    switch (hint) { 
+	    case 1:
+		if(computerChoice % 2 == 0) std::cout << "Nr este par";
+		else std::cout << "Nr este impar";
+	    case 2:
+		if(computerChoice > ((int)(final_score * difficulty)/2)) std::cout << "Numarul este mai mare decat " << (int)(final_score * difficulty)/2;
+		else std::cout << "Nr este mai mic decat " << (int)(final_score * difficulty)/2;
+	    case 3:
+		patrat_perfect(computerChoice);
+	    case 4:
+		if(find(numere_prime.begin(), numere_prime.end(),computerChoice) != numere_prime.end())	
+		    std::cout << "Nr este prim";
+		else std::cout << "Nr nu este prim";
+	    case 5:
+		if(computerChoice == 2) std::cout << "Nr este reprezentat de restantele lu A";
+		    
+	}
+
+  }
+
+  void roundResult(int PlayerChoice, int ComputerChoice) {
     std::cout << "Calculatorul a ales: " << ComputerChoice << "\n";
     if (DecideWin(PlayerChoice, ComputerChoice)) {
       std::cout << "Ai castigat\n";
@@ -38,85 +73,97 @@ private:
       incercari--;
       std::cout << "Ai gresit mai ai " << incercari << " incercari\n";
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000 ));
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     clearscreen();
   }
 
-  void display_Score()  {
+  void display_Score() {
     std::cout << "========= Scor ==========\n";
-    std::cout << "          " << current_score << "/" << final_score*difficulty << "\n";
+    std::cout << "          " << current_score << "/"
+              << final_score * difficulty << "\n";
   }
   bool isGameOver() {
-	if(incercari == 0 && current_score >= (final_score * difficulty)/2) return !last_stand();	
-	if(incercari == 0) return true;
-	return current_score >= final_score;
-    }
+    if (incercari == 0 && current_score >= (final_score * difficulty) / 2)
+      return !last_stand();
+    if (incercari == 0)
+      return true;
+    return current_score >= final_score;
+  }
   int getComputerChoice() {
     std::uniform_int_distribution<int> dist(1, (difficulty * number_range));
     return dist(gen);
   }
 
-  void endgame(){ 
-	std::cout << "\n===== Joc terminat =====\n"; 
-	std::cout << "Ai Ghicit " << current_score << " din " << final_score*difficulty << " numere\n";
-	if(current_score == final_score){
-	    std::cout << "Ai castigat " << prize*difficulty  << "$\n";
-	}else std::cout << "Ai castigat multa muie\n";
-	
-	bool rematch;
-	std::cout << "Doriti sa va mai incercati norocul inca o data? [1 = Da / 0 = Nu]"; std::cin>>rematch;
-	if(rematch)
-	    restart();
-	else
-	    std::cout << "Fraier\n";
-    }
-    void restart(){
-	current_score = 0;
-	incercari = 3;
-	stand = 1;
-	clearscreen();
-	start();
-    }
-    bool last_stand(){
-	if(stand == 1){
-	std::cout << "Sacrificati o parte din premiu pentru o incercare in plus? [1 = Da / 2 = Nu]";
-	bool decizie; std::cin >> decizie;
+  void endgame() {
+    std::cout << "\n===== Joc terminat =====\n";
+    std::cout << "Ai Ghicit " << current_score << " din "
+              << final_score * difficulty << " numere\n";
+    if (current_score == final_score) {
+      std::cout << "Ai castigat " << prize * difficulty << "$\n";
+    } else
+      std::cout << "Ai castigat multa muie\n";
+
+    bool rematch;
+    std::cout
+        << "Doriti sa va mai incercati norocul inca o data? [1 = Da / 0 = Nu]";
+    std::cin >> rematch;
+    if (rematch)
+      restart();
+    else
+      std::cout << "Fraier\n";
+  }
+  void restart() {
+    current_score = 0;
+    incercari = 3;
+    stand = 1;
+    clearscreen();
+    start();
+  }
+  bool last_stand() {
+    if (stand == 1) {
+      std::cout << "Sacrificati o parte din premiu pentru o incercare in plus? "
+                   "[1 = Da / 2 = Nu]";
+      bool decizie;
+      std::cin >> decizie;
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	if(decizie){
-	    prize  /= 2;
-	    incercari = 1;
-	    stand--;
-	    return true;
-	    }
-	 }else return false;
-	return false;
-    }
+      if (decizie) {
+        prize /= 2;
+        incercari = 1;
+        stand--;
+        return true;
+      }
+    } else
+      return false;
+    return false;
+  }
+
 public:
-    void start(){
-	clearscreen();
-	std::cout << "\n====Casino guessing game====\n";
-	std::cout << "Selectati o dificultate [1,2,3]  ";
-	int diff;
-	while(!(std::cin>>diff) || diff < 1 || diff >3){
-	    std::cout << "--Sangele a avut un singur job de facut--\n";
-	    std::cout << "--Introdu un nr valid [1,2,3]---\n";
-	    std::cin.clear();
-	    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	}
-	SetDifficulty(diff);
-	clearscreen();
-	   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	while(!isGameOver()){
-	    display_Score();
-	   int playerChoice = getPlayerChoice();
-	   int computerChoice = getComputerChoice();
-	   roundResult(playerChoice, computerChoice);
-	}
-	endgame();
+  void start() {
+    clearscreen();
+    std::cout << "\n====Casino guessing game====\n";
+    std::cout << "Selectati o dificultate [1,2,3]  ";
+    int diff;
+    while (!(std::cin >> diff) || diff < 1 || diff > 3) {
+      std::cout << "--Sangele a avut un singur job de facut--\n";
+      std::cout << "--Introdu un nr valid [1,2,3]---\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
+    SetDifficulty(diff);
+    clearscreen();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    while (!isGameOver()) {
+      display_Score();
+      int playerChoice = getPlayerChoice();
+      computerChoice = getComputerChoice();
+      roundResult(playerChoice, computerChoice);
+    }
+    endgame();
+  }
 };
-int main(){
-    Game game;
-    game.start();
-    return 0;
+
+int main() {
+  Game game;
+  game.start();
+  return 0;
 }
